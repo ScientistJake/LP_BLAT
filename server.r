f97aa26622eb992d1c3a27c4657edb5e330dbe53
Jacob Warner <warner.jacob@gmail.com>
2019-03-12 23:30:12 -0400

    first commit

---
+++ server.r
@@ -0,0 +1,84 @@
+library(plyr)
+library(dplyr)
+library(DT)
+
+server <- function(input, output, session){
+  
+  s <- reactiveValues(dataReady = "Data not reaedy...")
+  
+  blastresults <- eventReactive(input$blast, {
+    
+    #gather input and set up temp file
+    query <- input$query
+    tmp <- tempfile(fileext = ".fa")
+
+    #this makes sure the fasta is formatted properly
+    if (startsWith(query, ">")){
+      writeLines(query, tmp)
+    } else {
+      writeLines(paste0(">Query\n",query), tmp)
+    }
+    
+    #calls the blast
+    system(paste0("./blat ./data/lytechinus_pictus_30Nov2018_OWxax.fasta ",tmp," -out=blast8 ",tmp,".txt"), intern = T)
+    blat_table <- read.table(file = paste0(tmp,'.txt'),sep = '\t', quote="", skip=5)
+    colnames(blat_table) = c("Query","Target","%ID","alignment length","mismatches","gap openings","query start","query end","subject start","subject end","e-value","bit score")
+    return(blat_table)
+  }, ignoreNULL= T)
+
+  #makes the datatable
+  output$blastResults <- renderDataTable({
+    if (is.null(blastresults())){
+    } else {
+      blastresults()
+    }
+  }, selection="single")
+
+  observeEvent(input$blast, {
+    s$prepareData = "Data not reaedy..."
+  })
+  
+  psl <- eventReactive(input$export, {
+    
+    #gather input and set up temp file
+    query <- input$query
+    tmp <- tempfile(fileext = ".fa")
+    
+    #this makes sure the fasta is formatted properly
+    if (startsWith(query, ">")){
+      writeLines(query, tmp)
+    } else {
+      writeLines(paste0(">Query\n",query), tmp)
+    }
+    
+    #calls the blast
+    system(paste0("./blat ./data/lytechinus_pictus_30Nov2018_OWxax.fasta ",tmp," -out=psl ",tmp,".psl"), intern = T)
+
+    ## Here we're returning just the file path  
+    return(paste0(tmp,".psl"))
+  }, ignoreNULL= T)
+  
+  observeEvent(!is.null(psl()), {
+    s$dataReady = "Download data below:"
+  })
+  
+  output$dataReady <- renderText(s$dataReady)
+  
+  output$downloadTxt <- downloadHandler(
+    filename = function() {
+      paste("lol.txt", sep = "")
+    },
+    content = function(file) {
+      write.table(blastresults(), file, row.names = FALSE)
+    }
+  )
+    
+  output$downloadPsl <- downloadHandler(
+    filename = function() {
+      paste("lol.psl", sep = "")
+    },
+    content = function(file) {
+      file.copy(psl(), file)
+    }
+  )
+}
---
+++ ui.r
@@ -0,0 +1,62 @@
+library(shinythemes)
+library(DT)
+library(shinyjs)
+
+ui <- navbarPage(
+  id = "navbar",
+  selected = 'home',
+  header = tagList(
+    tags$head(
+      tags$link(rel="stylesheet", type="text/css",href="style.css"),
+      tags$script(type="text/javascript", src = "busy.js")
+    )
+  ),
+  theme = shinytheme("cerulean"),
+  title="LP-BLAT",
+  tabPanel("BLAT",value = "home",
+    #This block gives us all the inputs:
+    mainPanel(
+      headerPanel('LP BLAT!'),
+      textAreaInput('query', 'Input sequence:', value = "", placeholder = "", width = "600px", height="200px"),
+      actionButton("blast", "BLAT!")
+    ),
+                
+    #this snippet generates a progress indicator for long BLASTs
+    div(class = "busy",  
+      p("Calculation in progress.."), 
+      img(src="happyurchin.gif", align = "center")
+    ),
+                
+    #Basic results output
+    mainPanel(
+      h4("Results"),
+      DT::dataTableOutput("blastResults"),
+      br(),
+      p("Download txt here:"),
+      downloadButton("downloadTxt", "Download txt"),                 
+      br(),
+      br(),
+      p("Click 'Export psl' to generate a psl file:"),
+      p("Only click Export once, the download button will appear when its ready."),
+      actionButton('export', 'Export psl'),
+      br(),
+      textOutput("dataReady"),
+      conditionalPanel(
+        condition = "output.dataReady == 'Download data below:'",
+        downloadButton("downloadPsl", "Download psl")
+      )
+    )
+  ),
+  tabPanel("About",value = "about",
+    h3("About:"),
+    p("LP BLAT is a BLAT wrapper for the Lytechinus pictus genome assembly sequenced and assembled by the Lyons lab at the Scripps Institute of Oceanography."),
+    h3("Usage:"),
+    p("Paste a sequence or several in fasta format in the entry box and click BLAT. 
+      The alignment is slow so be patient. Once completed a table displaying the alignments will generate. 
+      You can download this table by clicking the 'Download txt' button.
+      To download a psl file that can be directly loaded into IGV, click Export psl ONLY ONCE. It will build the psl file and generate a download button upon completion."
+    ),
+    h3("Bugs:"),
+    p("Report bugs to: warnerj atsymbol uncw.edu")
+  )  
+)
