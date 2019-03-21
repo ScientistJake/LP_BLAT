4e7542f7015bc8abe11085d8c614e88afd2ad4d5
ScientistJake <warner.jacob@gmail.com>
2019-03-21 13:41:22 -0400

    Update server.r

--- server.r
+++ server.r
@@ -6,7 +6,7 @@ server <- function(input, output, session){
   
   s <- reactiveValues(dataReady = "Data not reaedy...")
   
-  blastresults <- eventReactive(input$blast, {
+  psl <- eventReactive(input$blast, {
     
     #gather input and set up temp file
     query <- input$query
@@ -20,56 +20,31 @@ server <- function(input, output, session){
     }
     
     #calls the blast
-    system(paste0("./blat ./data/lytechinus_pictus.2bit ",tmp," -out=blast8 ",tmp,".txt"), intern = T)
-    blat_table <- read.table(file = paste0(tmp,'.txt'),sep = '\t', quote="", skip=5)
-    colnames(blat_table) = c("Query","Target","%ID","alignment length","mismatches","gap openings","query start","query end","subject start","subject end","e-value","bit score")
-    return(blat_table)
-  }, ignoreNULL= T)
-
-  #makes the datatable
-  output$blastResults <- renderDataTable({
-    if (is.null(blastresults())){
-    } else {
-      blastresults()
-    }
-  }, selection="single")
-
-  observeEvent(input$blast, {
-    s$prepareData = "Data not reaedy..."
-  })
-  
-  psl <- eventReactive(input$export, {
-    
-    #gather input and set up temp file
-    query <- input$query
-    tmp <- tempfile(fileext = ".fa")
-    
-    #this makes sure the fasta is formatted properly
-    if (startsWith(query, ">")){
-      writeLines(query, tmp)
-    } else {
-      writeLines(paste0(">Query\n",query), tmp)
-    }
-    
-    #calls the blast
-    system(paste0("./blat ./data/lytechinus_pictus.2bit ",tmp," -out=psl ",tmp,".psl"), intern = T)
-
-    ## Here we're returning just the file path  
+    system(paste0("./blat ./data/lytechinus_pictus.2bit ",tmp," ",tmp,".psl"), intern = T)
     return(paste0(tmp,".psl"))
   }, ignoreNULL= T)
   
-  observeEvent(!is.null(psl()), {
-    s$dataReady = "Download data below:"
+  blat_results <-reactive({
+    system(paste0("perl pslScore.pl ",psl()," > ",psl(),".scored"), intern = T)
+    blat_table <- read.table(file = paste0(psl(),'.scored'),sep = '\t', quote="", skip=5)
+    colnames(blat_table) = c("Target","Target_start","Target_end","Query","score","percent Identity")
+    return(blat_table)
   })
-  
-  output$dataReady <- renderText(s$dataReady)
+
+  #makes the datatable
+  output$blastResults <- renderDataTable({
+    if (is.null(blat_results())){
+    } else {
+      blat_results()
+    }
+  }, selection="single")
   
   output$downloadTxt <- downloadHandler(
     filename = function() {
       paste("lol.txt", sep = "")
     },
     content = function(file) {
-      write.table(blastresults(), file, row.names = FALSE)
+      write.table(blat_results(), file, row.names = FALSE, sep='\t', quote=FALSE)
     }
   )
     
